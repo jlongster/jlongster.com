6bab08fef03c7a3ead0953518262433fc02f0f63
James Long <longster@gmail.com>
2024-04-23 14:03:34 -0400

    snap

--- app/routes/interactive/demo.tsx
+++ app/routes/interactive/demo.tsx
@@ -1,5 +1,9 @@
-import { forwardRef, useState, useEffect, useRef } from 'react';
+import { forwardRef, useState, useEffect, useRef, useMemo } from 'react';
 import bg from '../../bg.jpg';
+import { difference } from '../../poly';
+import { getPathFromCircleGroup } from '../../metaballs';
+import { lineCircleIntersection } from '../../intersection';
+import { Bezier } from 'bezier-js';
 
 function vec2len([x, y]) {
   return Math.sqrt(x * x + y * y);
@@ -33,6 +37,10 @@ function vec3add(v1, v2) {
 function vec3normalize(v) {
   const length = vec3len(v);
   return [v[0] / length, v[1] / length, v[2] / length];
+}
+
+function vec4add(v1, v2) {
+  return [v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2], v1[3] + v2[3]];
 }
 
 function mat4multvec3(mat, [x, y, z]) {
@@ -180,6 +188,7 @@ function debugLine(name, v1, v2, color = 'blue') {
   if (el == null) {
     el = document.createElement('div');
     el.id = name;
+    el.className = 'debug-render';
     Object.assign(el.style, {
       position: 'absolute',
       borderTop: '1px solid ' + color,
@@ -200,17 +209,21 @@ function debugLine(name, v1, v2, color = 'blue') {
   el.style.transform = `rotateZ(${angle}rad)`;
 }
 
-function debugShape(name, line1, line2, line3, line4) {
-  debugLine(name + '-1', line1[0], line1[1]);
-  debugLine(name + '-2', line2[0], line2[1]);
-  debugLine(name + '-3', line3[0], line3[1]);
-  debugLine(name + '-4', line4[0], line4[1]);
+function debugShape(name, points, color) {
+  let lastPoint = null;
+  points.forEach((point, idx) => {
+    if (lastPoint) {
+      debugLine(name + '-' + idx, lastPoint, point, color);
+    }
+    lastPoint = point;
+  });
 }
 
 function debugCircle(name, vec, size = 20) {
   let el = document.getElementById(name);
   if (el == null) {
     el = document.createElement('div');
+    el.className = 'debug-render';
     el.id = name;
     Object.assign(el.style, {
       position: 'absolute',
@@ -227,9 +240,154 @@ function debugCircle(name, vec, size = 20) {
   el.style.left = vec[0] - size / 2 + 'px';
 }
 
+function Svgs() {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const circles = [
+      {
+        cx: (Math.random() * 800) | 0,
+        cy: (Math.random() * 200) | 0,
+        r: 64,
+      },
+      {
+        cx: (Math.random() * 800) | 0,
+        cy: (Math.random() * 200) | 0,
+        r: 96,
+      },
+      {
+        cx: Math.random() * 800,
+        cy: Math.random() * 200,
+        r: 56,
+      },
+      // {
+      //   cx: Math.random() * 800,
+      //   cy: Math.random() * 200,
+      //   r: 128,
+      // },
+      // {
+      //   cx: Math.random() * 800,
+      //   cy: Math.random() * 200,
+      //   r: 76,
+      // },
+    ];
+    const path = getPathFromCircleGroup(circles);
+    let y = circles.map(circle => circle.cy + circle.r);
+    let x = circles.map(circle => circle.cx + circle.r);
+    const viewBox = `0 0 ${Math.round(Math.max(...x))} ${Math.round(
+      Math.max(...y),
+    )}`;
+
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    svg.setAttribute('version', '1.1');
+    svg.setAttribute('viewBox', viewBox);
+    svg.setAttribute('width', '800px');
+    svg.setAttribute('height', '200px');
+    svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
+    Object.assign(svg.style, {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      zIndex: 2,
+    });
+
+    const p = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    p.setAttribute('fill', 'red');
+    p.setAttribute('d', path);
+    svg.appendChild(p);
+
+    ref.current.appendChild(svg);
+
+    let last = null;
+    function render(timestamp) {
+      const dt = timestamp ? timestamp - (last || timestamp) : 0;
+
+      circles.forEach(circle => {
+        circle.cx += (Math.random() * 0.4 - 0.2) * dt;
+        circle.cy += (Math.random() * 0.4 - 0.2) * dt;
+      });
+      p.setAttribute('d', getPathFromCircleGroup(circles));
+
+      requestAnimationFrame(render);
+      last = timestamp;
+    }
+
+    render();
+  }, []);
+
+  return <div ref={ref} />;
+}
+
+function SvgFilter() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="0" height="0">
+      <defs>
+        <filter id="blur">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="6" />
+        </filter>
+        <filter id="contrast">
+          <feColorMatrix
+            in="blur"
+            mode="matrix"
+            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 15 -6"
+          />
+        </filter>
+        <filter id="gooey">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
+          <feColorMatrix
+            in="blur"
+            mode="matrix"
+            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 15 -6"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
+
 const Box3d = forwardRef((props, ref) => {
+  const blobs = useMemo(() =>
+    new Array(150).fill().map(_ => ({
+      size: Math.random() * 75 + 25,
+      x: Math.random() * 800,
+      y: Math.random() * 300,
+      duration: Math.random() * 4 + 1,
+      dir: [Math.random() * 4 - 2, Math.random() * 4 - 2],
+      off: Math.random(),
+      top: Math.random() < 0.25,
+      offset: Math.random() * 10,
+    })),
+  );
+
   return (
     <div className="scene">
+      {/*<svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        width="100"
+        height="100"
+        viewBox="0 0 100 100"
+      >
+        <line
+          stroke="var(--green-2)"
+          x1="0"
+          y1="0"
+          x2="50"
+          y2="50"
+          strokeWidth="30"
+        />
+        <path
+          stroke="var(--purple-2)"
+          strokeWidth="2"
+          fill="none"
+          // d="M50 50Ha8 8 0 0 0 8-8 32 32 0 1 0-32 32h2.34a9.66 9.66 0 0 0 6.83-16.48l-4.69-4.69A1.66 1.66 0 0 1 37.65 40Zm"
+          d="M10 10v5l20 20"
+        />
+        </svg>*/}
+
+      {/*<Blobs blobs={blobs} rectRef={props.rectRef} />*/}
+      {/*<SvgFilter />*/}
       <div ref={ref} className="scene-inner">
         <div
           className="shape cuboid-1 cub-1"
@@ -362,13 +520,102 @@ const Box3d = forwardRef((props, ref) => {
   );
 });
 
+function makeBlob(blob, outline) {
+  const div = document.createElement('div');
+  div.className = 'blob';
+  Object.assign(div.style, {
+    width: blob.size + 'px',
+    height: blob.size + 'px',
+    backgroundColor: outline ? '#202020' : 'black',
+    borderRadius: blob.size + 'px',
+    willChange: 'transform',
+    zIndex: 1,
+  });
+  return div;
+}
+
+function Blobs({ blobs, rectRef }) {
+  const ref = useRef(null);
+  const outerRef = useRef(null);
+
+  useEffect(() => {
+    const refs = [];
+    for (let blob of blobs) {
+      const div = makeBlob(blob);
+      ref.current.appendChild(div);
+      refs.push({ ref: div, blob });
+
+      if (blob.top) {
+        const divOutline = makeBlob(blob, true);
+        divOutline.style.zIndex = 2;
+        outerRef.current.appendChild(divOutline);
+        refs.push({ ref: divOutline, blob });
+      }
+    }
+
+    let last = null;
+    function render(timestamp) {
+      const dt = timestamp ? timestamp - (last || timestamp) : 0;
+
+      if (rectRef.current) {
+        const b = new Bezier(
+          0,
+          0,
+          480,
+          30,
+          rectRef.current.width,
+          rectRef.current.height,
+        );
+
+        for (let ref of refs) {
+          const p = b.get(
+            Math.sin(timestamp / 2000 + ref.blob.off) * 0.5 + 0.5,
+          );
+          ref.ref.style.transform = `translate3d(${p.x}px, ${p.y}px, 0)`;
+        }
+      }
+
+      requestAnimationFrame(render);
+      last = timestamp;
+    }
+
+    render();
+  }, []);
+
+  return (
+    <div
+      ref={outerRef}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 60,
+        width: 690,
+        height: 280,
+        borderRadius: 20,
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          filter: 'brightness(100%) contrast(8000%)',
+          backgroundColor: 'white',
+          width: 800,
+          height: 280,
+        }}
+      >
+        <div ref={ref} style={{ filter: 'blur(20px)' }} />
+      </div>
+    </div>
+  );
+}
+
 export default function Demo() {
   const ref = useRef();
   const currentForce = useRef(0);
   const currentForceDir = useRef(vec2normalize([0.1, 1.0]));
   const midRef = useRef(null);
   const midCursor = useRef(vec2normalize([0.1, 1.0]));
-  const cursorRef = useRef(null);
+  const cursorRef = useRef([0, 0]);
   const rectRef = useRef(null);
 
   const MAX_DIST = 800;
@@ -381,7 +628,7 @@ export default function Demo() {
   useEffect(() => {
     if (ref.current) {
       const r = ref.current.getBoundingClientRect();
-      midRef.current = [r.x + r.width / 2, r.y + r.height / 2 + window.scrollY];
+      midRef.current = [r.x + r.width / 2, r.y + window.scrollY + r.height / 2];
 
       const parentRect = ref.current.offsetParent.getBoundingClientRect();
       const offset = {
@@ -399,18 +646,18 @@ export default function Demo() {
       const face = ref.current.querySelector('.foreground');
       const bounds = face.getBoundingClientRect();
 
-      const div = document.createElement('div');
-      Object.assign(div.style, {
-        position: 'absolute',
-        top: offset.y + 'px',
-        left: offset.x + 'px',
-        width: bounds.width + 'px',
-        height: bounds.height + 'px',
-        backgroundColor: 'red',
-        zIndex: 1,
-        borderRadius: '0.3em',
-      });
-      ref.current.parentNode.appendChild(div);
+      // const div = document.createElement('div');
+      // Object.assign(div.style, {
+      //   position: 'absolute',
+      //   top: offset.y + 'px',
+      //   left: offset.x + 'px',
+      //   width: bounds.width + 'px',
+      //   height: bounds.height + 'px',
+      //   backgroundColor: '#f0f0f0',
+      //   zIndex: 1,
+      //   borderRadius: '0.3em',
+      // });
+      // ref.current.parentNode.appendChild(div);
       // ref.current.style.border = '1px solid red';
     }
   }, []);
@@ -454,7 +701,7 @@ export default function Demo() {
     });
 
     document.body.addEventListener('click', e => {
-      addForce(20);
+      addForce(-20);
     });
 
     // Render a 3d line
@@ -476,16 +723,20 @@ export default function Demo() {
   useEffect(() => {
     let last = null;
     function updateForce(timestamp) {
+      for (let el of document.querySelectorAll('.debug-render')) {
+        el.remove();
+      }
+
       const dt = timestamp ? timestamp - (last || timestamp) : 0;
 
       if (currentForce.current > 0) {
         currentForce.current = Math.max(
-          currentForce.current - (dt / 1000) * 80,
+          currentForce.current - (dt / 1000) * 300,
           0,
         );
       } else if (currentForce.current < 0) {
         currentForce.current = Math.min(
-          currentForce.current + (dt / 1000) * 80,
+          currentForce.current + (dt / 1000) * 300,
           0,
         );
       }
@@ -494,18 +745,12 @@ export default function Demo() {
 
       const rev = currentForce.current < 0;
 
-      // mat4translate(
-      //   0,
-      //   -currentForce.current / 5,
-      //   Math.abs(currentForce.current) / 2,
-      // ),
-
       const transformMatrix = mat4mult(
         mat4perspective(2000),
         mat4mult(
           mat4translate(
             0,
-            -currentForce.current / 5,
+            currentForce.current,
             Math.abs(currentForce.current) / 2,
           ),
 
@@ -517,26 +762,22 @@ export default function Demo() {
           ),
         ),
       );
-      // const transformMatrix = mat4translate(0, -20, 0);
 
-      console.log(transformMatrix);
+      // const globalOff = [midRef.current[0], midRef.current[1], 0, 1];
+
+      // debugCircle(
+      //   'foo',
+      //   project2d(
+      //     mat4multvec4(transformMatrix, vec4add(globalOff, [0, 0, 0, 1])),
+      //   ),
+      // );
+
+      // debugCircle('foo2', globalOff);
 
       ref.current.style.transform = `
         matrix3d(${mat4transpose(transformMatrix).join(',')})
       `;
-
-      // ref.current.style.transform = `
-      //   perspective(2000px)
-      //   translate3d(0, 0, 20px)
-      // `;
-
-      const matrix = window.getComputedStyle(ref.current).transform;
-      const m = matrix.match(
-        /matrix3d\(([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+), ([-.e\d]+)\)/,
-      );
-      if (m) {
-        // console.log(m.slice(1, 17).map(n => parseFloat(n)));
-      }
+      // ref.current.style.display = 'none';
 
       const r = rectRef.current;
 
@@ -545,26 +786,45 @@ export default function Demo() {
       const bottomleft = [-r.width / 2, r.height / 2, 0, 1];
       const bottomright = [r.width / 2, r.height / 2, 0, 1];
 
-      // const mat = mat4transpose(m.slice(1, 17).map(n =>
-      // parseFloat(n)));
       const mat = transformMatrix;
       let a1 = mat4multvec4(mat, topleft);
-      // console.log(mat);
-      // console.log('foo', topleft, a1);
       let a2 = mat4multvec4(mat, topright);
       let a3 = mat4multvec4(mat, bottomleft);
       let a4 = mat4multvec4(mat, bottomright);
 
-      // console.log(mat, a1);
-      // const offset = [rectRef.current.x, rectRef.current.y, 0];
       const poff = midRef.current;
-      // const poff = [0, 0];
       a1 = vec2add(project2d(a1), poff);
       a2 = vec2add(project2d(a2), poff);
       a3 = vec2add(project2d(a3), poff);
       a4 = vec2add(project2d(a4), poff);
 
-      debugShape('shape', [a1, a2], [a2, a4], [a3, a1], [a3, a4]);
+      const shape1 = [a1, a2, a4, a3, a1];
+      const shape2 = [
+        [r.x, r.y],
+        [r.x + r.width, r.y],
+        [r.x + r.width, r.y + r.height],
+        [r.x, r.y + r.height],
+        [r.x, r.y],
+      ];
+
+      const toPoint = p => ({ x: p[0], y: p[1] });
+      const fromPoint = p => [p.x, p.y];
+
+      let segments = null;
+      try {
+        segments = difference([shape2.map(toPoint)], [shape1.map(toPoint)]);
+      } catch (e) {}
+
+      debugShape('shape', shape1, 'red');
+
+      if (segments) {
+        segments.forEach((segment, idx) => {
+          if (segment.length > 2) {
+            segment.push(segment[0]);
+          }
+          debugShape('diff-' + idx, segment.map(fromPoint));
+        });
+      }
 
       requestAnimationFrame(updateForce);
       last = timestamp;
@@ -583,7 +843,17 @@ export default function Demo() {
         minHeight: 2000,
       }}
     >
-      <Box3d ref={ref} />
+      <Box3d ref={ref} rectRef={rectRef} />
+      <canvas
+        style={{
+          width: 900,
+          height: 800,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          pointerEvents: 'none',
+        }}
+      />
 
       <div style={{ marginTop: 100 }}>
         <input
@@ -595,6 +865,7 @@ export default function Demo() {
         />{' '}
         {currentForce.current}
       </div>
+
       <div
         style={{
           marginTop: 10,
